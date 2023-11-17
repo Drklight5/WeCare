c900fa153f23af27362044c2c7bb275581a9db7c
Valeria <valeria050603@hotmail.com>
2023-11-17 14:49:23 -0600

    menu

--- nextjs-blog/pages/Menu.js
+++ nextjs-blog/pages/Menu.js
@@ -3,8 +3,10 @@ import React from 'react'
 function Menu(props) {
   return (
     <div className="navbar navbar-expand-lg bg-light">
-    menudfdf
-    dfdfd</div>
+      <a href="">Link1</a>
+      <a href="">Link2</a>
+      <a href="">Link3</a>
+    </div>
   )
 }
 
